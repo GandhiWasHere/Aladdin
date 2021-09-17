66b737281ce7339fa3a6cdbc4361f6f3960e12fd
Ofek Itach <mr.ofekitach@cs.colman.ac.il>
2021-09-17 09:46:39 +0300

    litle change

--- Alladin/Controllers/CustomersController.cs
+++ Alladin/Controllers/CustomersController.cs
@@ -59,7 +59,7 @@ namespace Alladin.Controllers
         // POST: Customers/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost("UploadFiles")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(
             [Bind("CustomerID,CustomerName,CustomerPassword,CustomerAddress,CustomerPhoneNumber,CartID,CustomerImage")] CustomerView model)
