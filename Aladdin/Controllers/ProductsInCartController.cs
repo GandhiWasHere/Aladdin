093c8ac5298d9a63cc1ba4c0d58cd588cf222fee
Ofek <mr.ofekitach@gmail.com>
2021-10-09 12:38:30 +0300

    product in cart

--- Aladdin/Controllers/ProductsInCartController.cs
+++ Aladdin/Controllers/ProductsInCartController.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+//remove thisS
 namespace Aladdin.Controllers
 {
     public class ProductsInCartController : Controller
