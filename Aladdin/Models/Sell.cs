706972043841916298a6e35476797a466126fa81
Ofek <mr.ofekitach@gmail.com>
2021-10-11 00:06:50 +0300

    checkout for carts and sells model for agg

---
+++ Aladdin/Models/Sell.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aladdin.Models
+{
+    public class Sell
+    {
+        public int ProductID { get; set; }
+        public int Quantity{ get; set; }
+    }
+}
